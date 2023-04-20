8f297c0f619d18587c4b37dc4690fea394574975
RubenF <rubenf@campus.technion.ac.il>
2023-04-20 17:06:27 +0300

    hey

--- IsraeliQueue.c
+++ IsraeliQueue.c
@@ -1,6 +1,31 @@
 #include "IsraeliQueue.h"
 
 
+IsraeliQueue IsraeliQueueCreate(FriendshipFunction *, ComparisonFunction, int, int){
+
+}
+
+IsraeliQueue IsraeliQueueClone(IsraeliQueue q){
+
+}
+
+void IsraeliQueueDestroy(IsraeliQueue){
+
+}
+
+IsraeliQueueError IsraeliQueueEnqueue(IsraeliQueue q, void* item){
+    Node new_node;
+    new_node->data= item;
+    new_node->next= q.item_tail;
+    q.tail= &new_node;
+}
+
+
+IsraeliQueueError IsraeliQueueAddFriendshipMeasure(IsraeliQueue q, FriendshipFunction
+friendships_function){
+
+
+}
 
 IsraeliQueueError IsraeliQueueUpdateFriendshipThreshold(IsraeliQueue q, int n_thresh){
     // TODO bdika
@@ -19,16 +44,7 @@ int IsraeliQueueSize(IsraeliQueue q){
 }
 
 
-IsraeliQueueError IsraeliQueueEnqueue(IsraeliQueue q, void* item){
-    Node new_node;
-    new_node->data= item;
-    new_node->next= q.item_tail;
-    q.tail= &new_node;
-}
 
-
-IsraeliQueueError IsraeliQueueAddFriendshipMeasure(IsraeliQueue q, FriendshipFunction
-friendships_function){
-
+void* IsraeliQueueDequeue(IsraeliQueue){
 
 }
